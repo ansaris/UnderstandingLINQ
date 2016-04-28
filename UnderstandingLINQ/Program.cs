174ce61da9c42cbe5f3a26c38023e91583634603
ansaris <salman.ansari.usa@gmail.com>
2016-04-28 12:37:09 -0500

    Create Car class and List of cars.

--- UnderstandingLINQ/Program.cs
+++ UnderstandingLINQ/Program.cs
@@ -10,6 +10,21 @@ namespace UnderstandingLINQ
     {
         static void Main(string[] args)
         {
+            List<Car> myCars = new List<Car>
+            {
+                new Car() { VIN="A1", Make="BMW", Model="550i", StickerPrice=-0 },
+                new Car() { VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=0 },
+
+            }
+            Console.ReadLine();
         }
+    }
+
+    class Car
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int StickerPrice { get; set; }
     }
 }
