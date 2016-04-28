49742fc0ae563df72a260eb6c5ba5dcab56e2a59
ansaris <salman.ansari.usa@gmail.com>
2016-04-28 13:07:20 -0500

    Create LINQ query and method.

--- UnderstandingLINQ/Program.cs
+++ UnderstandingLINQ/Program.cs
@@ -14,8 +14,24 @@ namespace UnderstandingLINQ
             {
                 new Car() { VIN="A1", Make="BMW", Model="550i", StickerPrice=-0 },
                 new Car() { VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=0 },
+                new Car() { VIN="C3", Make="BMW", Model="745li", StickerPrice=0 },
+                new Car() { VIN="D4", Make="Ford", Model="Escape", StickerPrice=0 },
+                new Car() { VIN="E5", Make="BMW", Model="55i", StickerPrice=0 }
 
+            };
+
+            // LINQ query
+            var bmws = from car in myCars
+                       where car.Make == "BMW"
+                       select car;
+
+            // LINQ method
+
+            foreach (var car in bmws)
+            {
+                Console.WriteLine("{0} {1}", car.Model, car.VIN);
             }
+
             Console.ReadLine();
         }
     }
@@ -25,6 +41,7 @@ namespace UnderstandingLINQ
         public string VIN { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
-        public int StickerPrice { get; set; }
+        public int Year { get; set; }
+        public double StickerPrice { get; set; }
     }
 }
