4d2d5ff02238e19eb01f6c7d3bbff14613042aaa
ansaris <salman.ansari.usa@gmail.com>
2016-04-28 14:26:21 -0500

    Update LINQ method.

--- UnderstandingLINQ/Program.cs
+++ UnderstandingLINQ/Program.cs
@@ -12,20 +12,24 @@ namespace UnderstandingLINQ
         {
             List<Car> myCars = new List<Car>
             {
-                new Car() { VIN="A1", Make="BMW", Model="550i", StickerPrice=-0 },
-                new Car() { VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=0 },
-                new Car() { VIN="C3", Make="BMW", Model="745li", StickerPrice=0 },
-                new Car() { VIN="D4", Make="Ford", Model="Escape", StickerPrice=0 },
-                new Car() { VIN="E5", Make="BMW", Model="55i", StickerPrice=0 }
+                new Car() { VIN="A1", Make="BMW", Model="550i", StickerPrice=55000, Year = 2009 },
+                new Car() { VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=35000, Year = 2010 },
+                new Car() { VIN="C3", Make="BMW", Model="745li", StickerPrice=75000, Year = 2008 },
+                new Car() { VIN="D4", Make="Ford", Model="Escape", StickerPrice=25000, Year = 2008 },
+                new Car() { VIN="E5", Make="BMW", Model="55i", StickerPrice=57000, Year = 2010 }
 
             };
 
             // LINQ query
+            /*
             var bmws = from car in myCars
                        where car.Make == "BMW"
+                       && car.Year == 2010
                        select car;
+                       */
 
             // LINQ method
+            //var bmws = myCars.Where(p => p.Make == "BMW" && p.Year == 2010);
 
             foreach (var car in bmws)
             {
